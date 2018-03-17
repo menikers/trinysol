9a2189954080779dfe4972cb8a4a305c884f9365
menikers <menikers@gmail.com>
2018-03-17 16:09:54 -0600

    Initing aplication

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+port = process.env.PORT || 8080
+
+app.use(express.static (__dirname));
+
+
+app.get("/", function(req, res) {
+    res.render('ndex');
+})
+
+app.listen(port, function() {
+  console.log ("app runnig");
+})
+
+
+
+
+
